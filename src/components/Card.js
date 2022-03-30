51d51d7c723d4d223ebb5092d1b820309f8b4ce1
Lena Hou <lenahou2013@gmail.com>
2022-03-29 21:32:09 -0400

    adding style

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,19 +2,21 @@ import React from 'react';
 
 function Card(props){
     return(
+        <>
         <div className="card">
             <img src={props.card.thumbnail} alt={props.card.country} className="card--thumb"/>
             <div className="card--stats">
                 <div className="card--position">
                     <img src="../images/path-icon.png" alt="path icon" className="card--path" />
-                    <span className="position-text" >{props.card.country} </span>
+                    <p className="position-text" >{props.card.country}  <span className="google-text">View on Google Map</span></p>
                 </div>
-                <h1>{props.card.attraction}</h1>
-                <small>{props.card.startingDate} - {props.card.endDate}</small>
-                <p className="card--text">{props.card.description}</p> 
+                <h1 className="card--name">{props.card.attraction}</h1>
+                <small className="card--date">{props.card.startingDate} - {props.card.endDate}</small>
+                <p className="card--text">{props.card.description}</p>
             </div>
-
         </div>
+        <hr className="line" />
+        </>
     )
 
 }
